14489dff7e42499e2e39df2e1e6f1d10374bcd74
Ekylate <jt.antoine@gmail.com>
2023-03-22 22:08:31 +0100

    [JTA/JAE] : Finalisation d'un prototype limité en CLI
    [JTA/JAE] : Finalisation d'un prototype limité en CLI

--- src/main/launcher/Maestro.java
+++ src/main/launcher/Maestro.java
@@ -3,6 +3,11 @@ package launcher;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.util.Objects;
+
+import org.apache.commons.lang3.StringUtils;
+
+import services.IOConsoleService;
 
 /**
  * @author Sceok
@@ -10,17 +15,30 @@ import java.awt.event.KeyEvent;
  */
 public class Maestro {
 
+	private static Integer intervalsDuration;
+
+	private static Integer numberOfRepetitions;
+
+
 	/**
 	 * @param args
 	 * @throws AWTException
 	 */
 	public static void main(String[] args) throws AWTException {
+		displayWelcomingText();
+		askIntervalOfRepetitions();
+		askNumberOfRepetitions();
+		launchLoop();
+		displayEndingText();
+	}
+
+	private static void launchLoop() throws AWTException {
 		final Robot robot = new Robot();
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < numberOfRepetitions; i++) {
 			typeKeyWithRobot(robot, KeyEvent.VK_WINDOWS);
 			robot.delay(1000);
 			typeKeyWithRobot(robot, KeyEvent.VK_ESCAPE);
-			robot.delay(5000);
+			robot.delay(intervalsDuration);
 		}
 	}
 
@@ -29,4 +47,34 @@ public class Maestro {
 		robot.keyRelease(intKeyToType);
 	}
 
+	private static void displayWelcomingText() {
+		IOConsoleService.displayMessageInConsole("Pourquoi tu veux me faire travailler ? :( :(");
+	}
+
+	private static void askNumberOfRepetitions() {
+		IOConsoleService.displayMessageInConsole("Combien de fois dois-je travailler ?");
+		final String fetchedDataFromConsole = IOConsoleService.fetchDataFromConsole();
+		numberOfRepetitions = fetchIntegerFromString(fetchedDataFromConsole);
+	}
+
+	private static void askIntervalOfRepetitions() {
+		IOConsoleService.displayMessageInConsole("Je dois travailler toutes les X secondes : ?");
+		final String fetchedDataFromConsole = IOConsoleService.fetchDataFromConsole();
+		intervalsDuration = fetchIntegerFromString(fetchedDataFromConsole);
+		if(Objects.nonNull(intervalsDuration)) {
+			intervalsDuration *= 1000;
+		}
+	}
+
+	private static Integer fetchIntegerFromString(String stringParam) {
+		if (StringUtils.isNotBlank(stringParam)) {
+			return Integer.parseInt(stringParam);
+		}
+		return null;
+	}
+
+	private static void displayEndingText() {
+		IOConsoleService.displayMessageInConsole("Aaaah ! Enfin ! J'en avais marre de travailler !");
+		IOConsoleService.displayMessageInConsole("Allez, ciao !");
+	}
 }
