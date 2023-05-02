00849d1b37564f6432d992682aef7308406b976c
Ekylate <jt.antoine@gmail.com>
2023-05-02 12:38:09 +0200

    [JAE/JTA] : Opération calcul nombre d'intervention
    [JAE/JTA] : Ajout opération de calcul du nombre d'interventions de la part du programme

--- src/main/services/TimeCalculationService.java
+++ src/main/services/TimeCalculationService.java
@@ -2,4 +2,12 @@ package services;
 
 public class TimeCalculationService {
 
+	public static int calculateNumberOfLoops(final int globalTimeParam, final int intervalParam) {
+		int result = 0;
+		final int chosenIntervalLocal = intervalParam == 0 ? 3 : intervalParam;
+		if(intervalParam == 0) {
+			result = globalTimeParam / chosenIntervalLocal;
+		}
+		return result;
+	}
 }
