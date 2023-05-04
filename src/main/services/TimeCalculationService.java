aca598c540f6378ccd09ae7f42afac9361f91637
Ekylate <jt.antoine@gmail.com>
2023-05-04 14:20:15 +0200

    [JTA/JAE] : Correction condition + Ajout Javadoc
    [JTA/JAE] : Correction condition + Ajout Javadoc

--- src/main/services/TimeCalculationService.java
+++ src/main/services/TimeCalculationService.java
@@ -2,10 +2,16 @@ package services;
 
 public class TimeCalculationService {
 
+	/**
+	 * Calcule le nombre de répétitions à effectuer en fonction des deux paramètres de temps exprimés en secondes.
+	 * @param globalTimeParam nombre de secondes de la durée totale de travail.
+	 * @param intervalParam nombre de secondes entre chaque intervention/répétition.
+	 * @return
+	 */
 	public static int calculateNumberOfLoops(final int globalTimeParam, final int intervalParam) {
 		int result = 0;
 		final int chosenIntervalLocal = intervalParam == 0 ? 3 : intervalParam;
-		if(intervalParam == 0) {
+		if(chosenIntervalLocal != 0) {
 			result = globalTimeParam / chosenIntervalLocal;
 		}
 		return result;
